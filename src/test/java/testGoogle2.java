25f5a54f903726d4ee873e4eb67cda329a806eca
ralucaland <ralucaland@gmail.com>
2026-03-01 11:14:35 +0200

    Tema

---
+++ src/test/java/testGoogle2.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testGoogle2 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void searchTest() {
+        driver.findElement(By.xpath("//*[text()='Acceptă tot']")).click();
+
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.findElement(By.name("q")).submit();
+    }
+
+    @Test
+    public void searchWikipedia() {
+        driver.findElement(By.xpath("//*[text()='Acceptă tot']")).click();
+
+        driver.findElement(By.name("q")).sendKeys("Wikipedia");
+        driver.findElement(By.name("q")).submit();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
